9d378baf9e037c7b60dc78c0369bfd58403c39b7
Matt Garnes <matt@mattgarnes.com>
2012-08-23 00:57:39 -0400

    Added django code for the site. Relocated website files to a django templates folder.

---
+++ hh-server/heart_health/heart_health/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+	url(r'^$', 'survey.views.index', name='home'),
+    # Examples:
+    # url(r'^$', 'heart_health.views.home', name='home'),
+    # url(r'^heart_health/', include('heart_health.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
