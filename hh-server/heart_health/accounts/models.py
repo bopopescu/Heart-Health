420b1031861eef98394b9de2cfe257bae74e119e
Matt Garnes <matt@mattgarnes.com>
2012-09-14 15:09:49 -0400

    Fixed the UserProfile model to leave null objects for OneToOne Fields until they are populated later.

--- hh-server/heart_health/accounts/models.py
+++ hh-server/heart_health/accounts/models.py
@@ -5,11 +5,10 @@ from django.db.models.signals import post_save
       
 class UserProfile(models.Model):
     user = models.OneToOneField('auth.User')
-    user_survey = models.OneToOneField('survey.Survey')
     allow_notifications = models.BooleanField(default=True)
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
-    preferred_location = models.ForeignKey('survey.Location')
+    preferred_location = models.ForeignKey('survey.Location', null=True)
 
     class Admin:
         pass
