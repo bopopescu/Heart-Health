cd8a174f9d7bbda138ddf107bb9350aaf27afddd
Matt Garnes <matt@mattgarnes.com>
2012-12-19 22:37:13 -0500

    Updated settings.py for heroku deployment and added to requirements.txt

--- heart_health/heart_health/settings.py
+++ heart_health/heart_health/settings.py
@@ -9,17 +9,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'heart_health_db',                      # Or path to database file if using sqlite3.
-        'USER': 'matt',                      # Not used with sqlite3.
-        'PASSWORD': 'killerapp',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
-
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
@@ -179,3 +168,6 @@ LOGGING = {
 AUTHENTICATION_BACKENDS = ('django.contrib.auth.backends.ModelBackend',)
 
 AUTH_PROFILE_MODULE = 'survey.UserProfile'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
