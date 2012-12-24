14e064c35758b629cd83da26aa4f71ffe6017c2d
Matt Garnes <matt@mattgarnes.com>
2012-12-24 18:13:57 -0500

    Fix problem with path to static files.

--- heart_health/heart_health/settings.py
+++ heart_health/heart_health/settings.py
@@ -89,7 +89,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR, 'templates', 'media'),
+    os.path.join(PROJECT_DIR, '..', 'templates', 'media'),
 )
 
 # List of finder classes that know how to find static files in
@@ -130,7 +130,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR, 'templates', 'static'),
+    os.path.join(PROJECT_DIR, '..', 'templates', 'static'),
 )
 
 INSTALLED_APPS = (
